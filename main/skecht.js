9f891bea4f9409fd65aa467b52447da16ae80167
debianguaigua-2007 <debianguaigua@gmail.com>
2025-09-12 11:50:55 +0000

    1.0.4

--- main/skecht.js
+++ main/skecht.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
         const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
-    })
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+    });
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const alternaContraste = document.getElementById('alterna-contraste');
@@ -16,32 +16,32 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
         if(tamanhoAtualFonte > 5){
             tamanhoAtualFonte = 5;
             messageFonte.textContent = "Limite máximo de fonte atingido";
-        }else{messageFonte.textContent = `Tamanho aumentado para ${tamanhoAtualFonte}`}
-    })
+        }else{messageFonte.textContent = `Tamanho aumentado para ${tamanhoAtualFonte}`};
+    });
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
         if(tamanhoAtualFonte < 0.2){
             tamanhoAtualFonte = 0.2;
             messageFonte.textContent = "Limite mínimo de fonte atingido";
-        }else{messageFonte.textContent = `Tamanho diminuido para ${tamanhoAtualFonte}`}
-    })
+        }else{messageFonte.textContent = `Tamanho diminuido para ${tamanhoAtualFonte}`};
+    });
     alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste');
-    })
+    });
     reiniciarFonte.addEventListener('click', function(){
         tamanhoAtualFonte = 1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
         messageFonte.textContent = "";
-    })
+    });
     alternaFundo.addEventListener('click', function(){
         document.querySelector('main').classList.toggle('sem-fundo');
-    })
-})
+    });
+});
 ScrollReveal().reveal('#Home', { delay: 500 });
 ScrollReveal().reveal('#News', { delay: 500 });
 ScrollReveal().reveal('#Contact', { delay: 500 });
